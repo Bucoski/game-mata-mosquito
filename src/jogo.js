3ea6cacec27850829922917fc67ff9134e336509
Wagner Bucoski <bucoski.wagner@gmail.com>
2025-02-21 17:45:01 -0300

    random sized mosquitos

--- src/jogo.js
+++ src/jogo.js
@@ -21,12 +21,13 @@ function posicaoRandomica() {
     [posicaoX, posicaoY] = validaPosicao(posicaoX, posicaoY);
 
 
-    var mosquito = document.createElement('img')
-    mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
-    mosquito.style.left = posicaoX + 'px'
-    mosquito.style.top = posicaoY + 'px'
-    mosquito.style.position = 'absolute'
+    var mosquito = document.createElement('img');
+
+    mosquito.src = 'imagens/mosquito.png';
+    mosquito.className = tamanhoAleatorio();
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute';
 
     document.body.appendChild(mosquito)
 }
@@ -37,4 +38,19 @@ function validaPosicao(x, y) {
     y = y < 0 ? 0 : y;
 
     return [x, y];
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+        case 3:
+            return 'mosquito4'
+    }
 }
